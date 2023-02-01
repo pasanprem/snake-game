dcc4e271fcb01fe164b902c4e23b1d1895be9a45
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-02-01 11:03:02 +0000

    Basic movement for the head and the body done.

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-all_turtles = []
+segments = []
 
 for _ in range(3):
     t = Turtle(shape="square")
     t.color("white")
     t.penup()
     t.goto(x= ( _ * 10), y=0)
+    segments.append(t)
+
+# screen.update()
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) -1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+
+    segments[0].forward(20)
+    segments[0].left(90)
+
+
 
 
 
