3a32f6a61f8a74651d2460447ee2854279076c26
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-02-01 15:48:21 +0000

    Direction keys done.

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-
 snake = Snake()
-game_on = True
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
