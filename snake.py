1a794ce1b15f09839f83528096841611a7fc4a87
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-02-02 10:00:24 +0000

    Turn back restrictions implemented.

--- snake.py
+++ snake.py
@@ -1,7 +1,13 @@
 from turtle import Turtle
 
-STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
 
 class Snake:
 
@@ -9,6 +15,7 @@ class Snake:
 
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]  # Hardcode the head of the snake for easier use
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -16,24 +23,30 @@ class Snake:
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
-            self.segments.append((new_segment))
+            self.segments.append(new_segment)
 
     def move(self):
-        for seg_num in range(len(self.segments) -1, 0, -1):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
 
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
+    # The directions are based on the 'standard mode'
+    # at https://docs.python.org/3/library/turtle.html#turtle.setheading
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
