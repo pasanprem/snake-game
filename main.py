63039d9dc67d2c45d6759a949ecee18e2e060b96
pasanprem <46164432+pasanprem@users.noreply.github.com>
2023-02-02 21:59:00 +0000

    Basic functionality completed

--- main.py
+++ main.py
@@ -30,7 +30,20 @@ while game_on:
     # Detect collision with food
     if snake.head.distance(food) < 20:
         food.refresh()
+        snake.extend()
         score.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        score.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 15:
+            game_on = False
+            score.game_over()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-
+ALIGNMENT = "center"
+FONT = ('Arial', 20, 'normal')
 
 class Scoreboard(Turtle):
     def __init__(self):
@@ -11,7 +12,11 @@ class Scoreboard(Turtle):
 
     def write_score(self):
         self.clear()
-        self.write(f"Score: {self.score}", move=False, align='Left', font=('Arial', 20, 'normal'))
+        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over!", move=False, align=ALIGNMENT, font=FONT)
